a6d6aaa3229b1f409780ab2a0818b58082259ce8
Vignesh H <vignesh.h2004official@gmail.com>
2026-01-27 22:01:51 +0530

    If Else Program

--- If Else.py
+++ If Else.py
@@ -5,3 +5,13 @@
 #If  is even and in the inclusive range of 2 to 5, print Not Weird
 #If  is even and in the inclusive range of 6 to 20, print Weird
 # If  is even and greater than 20 , print Not Weird
+
+a = int(input().strip())
+if (a%2==1):
+    print ("Weird")
+elif (a > 2 and a < 5):
+    print ("Not Weird")
+elif (a > 5 and a < 20):
+    print ("Weird")
+else :
+    print ("Not Weird")
