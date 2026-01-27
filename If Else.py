fcdbdd3fe0edd22f0633979cb8fd6bb4a2ae6523
Vignesh H <vignesh.h2004official@gmail.com>
2026-01-27 22:04:20 +0530

    If Else Program - Hackerank (Python)

--- If Else.py
+++ If Else.py
@@ -1,8 +1,8 @@
 #Task
-#Given an integer, , perform the following conditional actions:
+# Given an integer, , perform the following conditional actions:
 # If  is odd, print Weird
-#If  is even and in the inclusive range of 2 to 5, print Not Weird
-#If  is even and in the inclusive range of 6 to 20, print Weird
+# If  is even and in the inclusive range of 2 to 5, print Not Weird
+# If  is even and in the inclusive range of 6 to 20, print Weird
 # If  is even and greater than 20 , print Not Weird
 
 a = int(input().strip())
