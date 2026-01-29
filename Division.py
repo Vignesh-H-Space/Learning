171fd104423cd5b6c4c73b35b4b6a9f54dca73fd
Vignesh H <vignesh.h2004official@gmail.com>
2026-01-29 21:42:10 +0530

    Initial Division Commit

--- Division.py
+++ Division.py
@@ -1,3 +1,5 @@
 # The provided code stub reads two integers,  and , from STDIN.
 
 # Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / 
+
+a = int(input())
