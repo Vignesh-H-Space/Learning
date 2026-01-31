ab1f560754408c58c934dab2e0450c6e2967f5a8
Vignesh H <vignesh.h2004official@gmail.com>
2026-01-31 12:38:13 +0530

    Number Guessing Game - Part 1

--- ProITBridge/Guessing Number 1.py
+++ ProITBridge/Guessing Number 1.py
@@ -1,6 +1,6 @@
-n = int(input("Enter a number between 1 to 100"))
+n = int(input("Enter a number between 1 to 100 "))
 if(n==56):
     print("You have guessed correct ")
-else :
+else:
     print("Wrong guess, kindly guess again")
     
