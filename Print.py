08d8685c9bdc1f6ef79085288cddbdbaae02f9d9
Vignesh H <vignesh.h2004official@gmail.com>
2026-01-31 10:50:39 +0530

    Print Program

---
+++ Print.py
@@ -0,0 +1,14 @@
+# The included code stub will read an integer, , from STDIN.
+
+# Without using any string methods, try to print the following:
+#123 ... n
+
+# Note that "" represents the consecutive values in between.
+
+# Example
+# n = 
+# Print the string 12345.
+
+n = int(input())
+for a in range(1,n+1):
+    print(a,end="")
