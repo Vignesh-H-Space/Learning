c1711579bf8f6d105dabcea950906b11102d96b7
Vignesh H <vignesh.h2004official@gmail.com>
2026-01-31 12:50:21 +0530

    Number Guessing Game - Part 2

---
+++ ProITBridge/Python/Guessing Number 2.py
@@ -0,0 +1,9 @@
+Secret_Number = 56
+for i in range (0,7):
+    n = int(input("Enter a number between 1 to 100 "))
+    if(n==Secret_Number):
+        print("You have guessed correct ")
+        break
+    else:
+        print("Wrong guess, kindly guess again")
+        
