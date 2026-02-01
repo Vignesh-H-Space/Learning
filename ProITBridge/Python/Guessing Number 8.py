f3b3c31c3f173b46e8d8e71e13f948b677a0946e
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-01 10:33:53 +0530

    Number Guessing Game - Part 8

--- ProITBridge/Python/Guessing Number 8.py
+++ ProITBridge/Python/Guessing Number 8.py
@@ -7,3 +7,23 @@
 # Should give options like Number is lesser or greater than guessed number 
 # Give total number of attempts to the user
 # Say that the total number of attempts exceeded 
+
+def number_guessing_game():
+        
+    import random 
+    Secret_Number = random.randint(1,100)
+    while(True):
+        try :
+            n = int(input("Enter a number between 1 to 100 \n"))
+            if(n>100 or n <1):
+                print("The number you have entered is not falling under 1 to 100. Kindly enter the number between the interval 1 to 100")
+            elif(n>Secret_Number):
+                print("The number you have entered is greater than the secret number")
+            elif(n<Secret_Number):
+                print("The number you have entered is lesser than the secret number")
+            else:
+                print(f"Your guess is correct")
+                break
+        except ValueError:
+            print("Kindly enter only numbers and that should be strictly between 1 to 100")
+
