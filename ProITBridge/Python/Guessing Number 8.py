69b862d38d722bec96442159024fdb8ee810a044
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-01 10:31:59 +0530

    Task Description

---
+++ ProITBridge/Python/Guessing Number 8.py
@@ -0,0 +1,9 @@
+# Guessing Number Part 8
+
+# Making the entire program as Function
+# Exception Handling - Try & Catch 
+# Generate Random Secret Number
+# Number should be 1-100, if it is not, then pop error 
+# Should give options like Number is lesser or greater than guessed number 
+# Give total number of attempts to the user
+# Say that the total number of attempts exceeded 
