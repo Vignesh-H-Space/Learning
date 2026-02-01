5263ab2f2c26506d5eb077c51f405815e042b01d
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-01 14:02:57 +0530

    Print Function

--- ProITBridge/Python/Print Function.py
+++ ProITBridge/Python/Print Function.py
@@ -1,1 +1,8 @@
 # Different Print Functions present in python 
+
+print ("Hello there, I'm Vignesh ")  # Using Single ' quotes
+
+print ('Hello there, I\'m Vignesh ') # Using the Escape Character
+
+print(""" Hello 
+      This is multi line print statement """)
