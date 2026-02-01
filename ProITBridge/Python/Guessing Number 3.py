49ebac2b351de69da8dd7c150a88c176eca30311
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-01 10:10:23 +0530

    Number Guessing Game - Part 3

--- ProITBridge/Python/Guessing Number 3.py
+++ ProITBridge/Python/Guessing Number 3.py
@@ -3,9 +3,9 @@
 
 Secret_Number = 56
 while True :
-    n = int(input("Enter a number between 1 to 100 "))
+    n = int(input("Enter a number between 1 to 100"))
     if(n==Secret_Number):
-        print("You have guessed correct ")
+        print("You have guessed correct")
         break
     else:
         print("Wrong guess, kindly guess again")
