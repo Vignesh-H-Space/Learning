3c3ad6da90ec2ba536073057ba5e104a8b06b688
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-01 14:52:52 +0530

    String - Concatenation & Indexing

---
+++ ProITBridge/Python/String - Concat & Indexing.py
@@ -0,0 +1,11 @@
+# String - Concatenation & Indexing 
+
+fname = 'Robert'
+lname = 'Downey'
+age = 45
+
+print (fname+" "+lname+"_"+str(age)) # Concatenation 
+
+print (fname,lname,age) # Normal Usage
+ 
+print (fname[0:3]) # Indexing first three words - 0,1,2
