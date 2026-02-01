6227363748613beed5264a93e89e05cc1d9d2173
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-01 10:13:11 +0530

    Number Guessing Game - Part 7

---
+++ ProITBridge/Python/Guessing Number 7.py
@@ -0,0 +1,8 @@
+# Guessing Number Part 7
+
+# Exception Handling - Try & Catch 
+# Generate Random Secret Number
+# Number should be 1-100, if it is not, then pop error 
+# Should give options like Number is lesser or greater than guessed number 
+# Give total number of attempts to the user
+# Say that the total number of attempts exceeded 
