be90c609b5a88b794f4affafc68930c789ff8b21
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-02 06:11:28 +0530

    String Print

--- ProITBridge/Guessing Number 1.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Enter a number between 1 to 100 "))
-if(n==56):
-    print("You have guessed correct ")
-else:
-    print("Wrong guess, kindly guess again")
-    
---
+++ ProITBridge/Python/String - Input.py
@@ -0,0 +1,12 @@
+# String - Input Function
+
+a = input("Enter your name ")
+b = input("Enter your age ")
+
+print ("Your name is "+a+" and your age is "+b)
+
+# Optimising the print 
+print ("Your name is",a,"and your age is",b)
+
+# Further Optimisation 
+print("Your name is {} and your age is {}".format(a,b))  # placeholder , # print formatting 
