3b41597a2f2d409189cc0d9d6dd63b472f3fb50b
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-02 21:44:16 +0530

    Positional Arguments in Functions

---
+++ ProITBridge/Python/Functions - 2.py
@@ -0,0 +1,8 @@
+# Positional Arguments in Function
+
+def get_details (name, place, age):  # Parameters 
+    print ("The customer name is",name)
+    print("The customer resides at",place)
+    print("The age of the customer is",age)
+
+get_details("Rahul","Chennai",23)  # Passing Arguments 
