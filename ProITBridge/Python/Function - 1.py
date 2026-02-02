36c641778751c28057485d1489446e293f3c3fe0
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-02 06:28:55 +0530

    Python - Functions -1

--- ProITBridge/Python/Function - 1.py
+++ ProITBridge/Python/Function - 1.py
@@ -1,1 +1,11 @@
 # Functions in Python 
+
+def welcome():
+    print ("Welcome to VS Code")
+    
+welcome()
+
+def addition (first_num, second_num, third_num):  # Parameters 
+    return first_num + second_num + third_num
+
+print(addition (10,20,50)) # Passing Arguments 
