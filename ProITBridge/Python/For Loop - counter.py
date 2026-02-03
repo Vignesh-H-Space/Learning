c21e2cf5d90bda762e636f3231bdcec9445c9c09
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-03 06:24:35 +0530

    For Loop - Step function (Counter)

---
+++ ProITBridge/Python/For Loop - counter.py
@@ -0,0 +1,7 @@
+# For loop counter 
+
+for i in range(0,10,3):  # 0 - Start, 10 - End, 3 - Step (Counter)
+    print(i)
+    
+for i in range(0,-10,-3):  # 0 - Start, -10 - End, -3 - Step (Counter)
+    print(i)
