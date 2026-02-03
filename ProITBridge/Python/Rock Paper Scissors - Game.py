b7e20830760edc1ba4d9923af797aa521e9e8362
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-03 21:38:58 +0530

    Description

---
+++ ProITBridge/Python/Rock Paper Scissors - Game.py
@@ -0,0 +1,20 @@
+# Rock Paper Scissors - Game 
+
+''' Objective:
+Build a simple Rock-Paper-Scissors game where the player competes with the computer.
+ This task will test your understanding of:
+●Input/output
+●Conditional logic
+●Random module
+●Loops
+●Exception handling|
+
+
+Instructions:
+1.The user should enter their choice: rock, paper, or scissors.
+2.The computer should randomly select one of these.
+3.Compare both and print who wins.
+4.Ask if the player wants to play again- if yes, repeat; if no, exit.
+5.Handle invalid inputs gracefully using try-except or condition checks. '''
+
+
