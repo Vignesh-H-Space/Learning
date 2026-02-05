0aa16634deb439d0357a2113ac6383bc91fd1f20
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-05 06:02:39 +0530

    Initial Description

---
+++ ProITBridge/Python/Word Guessing - Game.py
@@ -0,0 +1,18 @@
+# Word Guessing Game (Mini Hangman)
+''' 
+Objective:
+Create a word-guessing game where the player tries to guess the letters of a hidden word.
+
+This will test your:
+●Loops
+●Conditional logic
+●String manipulation
+●Exception handling
+●Logical thinking
+
+Instructions:
+1.Create a list of random words (like “python”, “machine”, “learning”, “bridge”).
+2.Randomly select one word for the game.
+3.Show the word as blanks (_ _ _ _) and let the player guess one letter at a time.
+4.Keep track of wrong guesses (max 6 attempts).
+5.End the game when the player either guesses the full word or uses all chances. '''
