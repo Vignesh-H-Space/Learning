48ce7357f2d33006e08247e87677fa0f6da77fc6
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-05 06:30:37 +0530

    Starting Commit

--- ProITBridge/Python/Word Guessing - Game.py
+++ ProITBridge/Python/Word Guessing - Game.py
@@ -16,3 +16,22 @@ Instructions:
 3.Show the word as blanks (_ _ _ _) and let the player guess one letter at a time.
 4.Keep track of wrong guesses (max 6 attempts).
 5.End the game when the player either guesses the full word or uses all chances. '''
+
+import random 
+
+word_list = ["python","machine","learning","bridge","number","function","investment"]
+word = random.choice(word_list)
+length = len(word)
+print("Welcome to Word Guessing Game ")
+print("You have 6 attemtps to guess the entire word ")
+computer_word = ["_"] * length
+print (computer_word)
+for i in range (0,6):
+    try : 
+        letter = input("Enter your first guess: ").lower()
+        if len(letter) > 1 : 
+            raise ValueError("Incorrect Input !!! ")
+        
+    
+    except ValueError:
+        print ("Enter only one letter at a time")
