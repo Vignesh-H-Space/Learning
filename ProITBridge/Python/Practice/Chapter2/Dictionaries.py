20d699e4ce162964047d6156d847a9e3b1a5eb9a
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-07 21:37:46 +0530

    Dictionaries in python

--- ProITBridge/Python/Practice/Chapter2/Dictionaries.py
+++ ProITBridge/Python/Practice/Chapter2/Dictionaries.py
@@ -1,1 +1,19 @@
-# Dictonaries in Python 
+# Dictionaries in Python 
+
+
+dictionary1 = {}  # Empty Dictionary
+print(dictionary1)
+
+print(type(dictionary1))
+
+
+items_to_buy4 = {"Apple":45,"Banana":50,"Orange":12,"Apple":17} # Duplicate keys are allowed but value get updated
+
+print(items_to_buy4)
+
+print(len(items_to_buy4))
+
+print(type(items_to_buy4))
+
+# print(items_to_buy4[0]) - ERROR - Dictionary doesnt allow indexing and slicing 
+
