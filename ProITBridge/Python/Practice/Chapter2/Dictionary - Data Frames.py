4609a94e8263820c7015d169efd02d3076f4ca5f
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-07 22:12:56 +0530

    Dictionary - Data Frames in Python

--- ProITBridge/Python/Practice/Chapter2/Dictionary - Data Frames.py
+++ ProITBridge/Python/Practice/Chapter2/Dictionary - Data Frames.py
@@ -2,6 +2,12 @@
 
 import pandas as pd 
 
+# Example 1
 data1 = {"Avengers":["Thor","Captain America","Hulk","Iron Man"],"Power":[90,85,95,80]}
 
 print(pd.DataFrame(data=data1))
+
+# Example 2
+data2 = {"Country":("India","USA","Russia","UK","Germany","France"),"Capital":("New Delhi","Washington DC","Moscow","London","Berlin","Paris")}
+
+print(pd.DataFrame(data=data2))
