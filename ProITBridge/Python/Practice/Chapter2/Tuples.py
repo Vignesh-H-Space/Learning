e3df2ef7fc64f1a8470c6d4a763b308b3fe7cbc3
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-07 20:40:08 +0530

    Tuples in python

--- ProITBridge/Python/Practice/Chapter2/Tuples.py
+++ ProITBridge/Python/Practice/Chapter2/Tuples.py
@@ -1,1 +1,31 @@
 # Tuples in Python
+
+tuple1 = ()  # Empty List
+print(tuple1)
+
+print(type(tuple1))
+
+
+items_to_buy2 = ("Apple","Pomogranate","Orange","Apple",12,78,91.1)
+print(items_to_buy2)
+
+print(len(items_to_buy2))
+
+print(type(items_to_buy2))
+
+print(items_to_buy2[3]) # indexing
+
+print(items_to_buy2[-4])   # negative indexing
+
+print(items_to_buy2[0:3])  # slicing  
+
+print(items_to_buy2[:3])  # slicing  
+
+print(items_to_buy2[3:6])  # slicing  
+
+# Returns empty tuple because slicing goes from left to right only
+print(items_to_buy2[-2:-5])  # negative slicing 
+
+print(items_to_buy2[-4:-1])  # negative slicing 
+
+# items_to_buy2[3] = "Wow"  ERROR - Tuple is immutable
