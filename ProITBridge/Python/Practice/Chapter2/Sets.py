e1613cfc8f8f13a4437bd06706649801405c1504
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-07 20:33:08 +0530

    Sets in python

--- ProITBridge/Python/Practice/Chapter2/Sets.py
+++ ProITBridge/Python/Practice/Chapter2/Sets.py
@@ -1,1 +1,18 @@
 # Set in Python 
+
+
+set1 = set()  # Empty Set
+print(set1)
+
+print(type(set1))
+
+items_to_buy1 = set(("Apple","Pomogranate","Orange","Apple",12,78,91.1)) # Set allows only one argument and it must be iterable like list, tuple or string
+
+# print(items_to_buy1[0]) - ERROR - set doesnt allow indexing and slicing 
+
+print(items_to_buy1) # Duplicate is not allowed and also auto indexing 
+
+print(len(items_to_buy1))
+
+print(type(items_to_buy1)) 
+
