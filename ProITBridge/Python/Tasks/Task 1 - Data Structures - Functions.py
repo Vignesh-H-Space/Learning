8359a07b353b11a97349af1bd48d3cb9451981e5
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-08 11:57:09 +0530

    List Functions

--- ProITBridge/Python/Tasks/Task 1 - Data Structures - Functions.py
+++ ProITBridge/Python/Tasks/Task 1 - Data Structures - Functions.py
@@ -6,6 +6,23 @@
 # List Functions
 
 items_to_buy = ["Apple","Pomogranate","Orange","Apple",12,78,91.1]
-
 items_to_buy.append("Watermelon")  # Append - Adds new element to the end of list 
 print(items_to_buy)
+
+
+items_to_buy = ["Apple","Pomogranate","Orange","Apple",12,78,91.1]
+items_to_buy.clear()  # Clear - Clears the entire list elements and only empty list appears 
+print(items_to_buy)
+
+
+items_to_buy = ["Apple","Pomogranate","Orange","Apple",12,78,91.1]
+items = items_to_buy.copy()  # Copy - Returns a shallow copy of list
+items.clear()
+print(items)
+print(items_to_buy)
+
+
+items_to_buy = ["Apple","Pomogranate","Orange","Apple",12,78,91.1]
+print(items_to_buy.count("Apple"))  # Count - Counts the total occurences of a particular element in list
+print(items_to_buy.count("Apples")) # Returns 0 if the element doesn't exist
+
