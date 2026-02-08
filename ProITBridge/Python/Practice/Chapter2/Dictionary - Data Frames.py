0f7214ca03fde0ed3f7eb0078477f730a3bfd352
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-08 10:38:40 +0530

    Dictionary - Data Frames in Python

--- ProITBridge/Python/Practice/Chapter2/Dictionary - Data Frames.py
+++ ProITBridge/Python/Practice/Chapter2/Dictionary - Data Frames.py
@@ -11,3 +11,9 @@ print(pd.DataFrame(data=data1))
 data2 = {"Country":("India","USA","Russia","UK","Germany","France"),"Capital":("New Delhi","Washington DC","Moscow","London","Berlin","Paris")}
 
 print(pd.DataFrame(data=data2))
+
+# Example 3 
+
+data3 = {"Players":("Messi","Ronaldo","Neymar","Mbappe","Haaland","Kane"),"Clubs":["Inter Miami","Al Nassr","Santos","Real Madrid","Man City","Bayern"],"Country":("Argentina","Portugal","Brazil","France","Norway","England")}
+
+print(pd.DataFrame(data=data3))
