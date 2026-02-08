ac1b74b15ec22247ce3afdfbe3cf5b8f3b310499
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-08 10:56:29 +0530

    Dictionary (Data Frames)

---
+++ ProITBridge/Python/Tasks/Task 1 - Data Structures - Functions.py
@@ -0,0 +1,3 @@
+# Data Structures 
+
+# List, Tuple, Set, Dictionary - One example for each functions 
--- ProITBridge/Python/Tasks/Task 2 - Dictionary (Data Frames).py
+++ ProITBridge/Python/Tasks/Task 2 - Dictionary (Data Frames).py
@@ -2,3 +2,22 @@
  
 # Dictionary 
 # Use Dictionaries to create Data Frames with rows and columns
+
+
+import pandas as pd 
+
+# Example 1
+data1 = {"Avengers":["Thor","Captain America","Hulk","Iron Man"],"Power":[90,85,95,80]}
+
+print(pd.DataFrame(data=data1))
+
+# Example 2
+data2 = {"Country":("India","USA","Russia","UK","Germany","France"),"Capital":("New Delhi","Washington DC","Moscow","London","Berlin","Paris")}
+
+print(pd.DataFrame(data=data2))
+
+# Example 3 
+
+data3 = {"Players":("Messi","Ronaldo","Neymar","Mbappe","Haaland","Kane"),"Clubs":["Inter Miami","Al Nassr","Santos","Real Madrid","Man City","Bayern"],"Country":("Argentina","Portugal","Brazil","France","Norway","England")}
+
+print(pd.DataFrame(data=data3))
