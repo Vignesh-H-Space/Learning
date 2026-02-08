8c96020ddd8183578aa78c31cecd8e3b83dcad50
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-08 11:39:54 +0530

    List Function - Append

--- ProITBridge/Python/Tasks/Task 1 - Data Structures - Functions.py
+++ ProITBridge/Python/Tasks/Task 1 - Data Structures - Functions.py
@@ -1,3 +1,11 @@
 # Data Structures 
 
 # List, Tuple, Set, Dictionary - One example for each functions 
+
+
+# List 
+
+items_to_buy = ["Apple","Pomogranate","Orange","Apple",12,78,91.1]
+
+items_to_buy.append("Watermelon")  # Append - Adds new element to the end of list 
+print(items_to_buy)
