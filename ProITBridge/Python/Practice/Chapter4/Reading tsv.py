adf7d7cf7c7982f3fe64c49d98ec1e4b67e607aa
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-09 22:04:51 +0530

    Read CSV to read TSV File

--- ProITBridge/Python/Practice/Chapter4/Reading tsv.py
+++ ProITBridge/Python/Practice/Chapter4/Reading tsv.py
@@ -1,3 +1,9 @@
 # Reading TSV (Tab Separate Values) in python 
 
 # Importing a file and reading it using pandas 
+
+import pandas as pd 
+
+pd.read_csv(filepath_or_buffer= "data.tsv.txt",sep = "\t")
+
+pd.read_table(filepath_or_buffer= "data.tsv.txt")
