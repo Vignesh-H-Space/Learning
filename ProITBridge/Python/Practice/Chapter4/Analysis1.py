2f71fa04f69747878a3445098967fc4487b19120
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-10 06:51:38 +0530

    Shape Attribute

---
+++ ProITBridge/Python/Practice/Chapter4/Analysis1.py
@@ -0,0 +1,12 @@
+
+# Initial Analysis
+# Using shape attribute 
+
+# Function()
+# Attribute 
+
+import pandas as pd 
+
+order_details = pd.read_table(filepath_or_buffer= "data.tsv.txt")
+
+print(order_details.shape)
