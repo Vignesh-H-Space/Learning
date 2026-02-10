793ea5d85db6cd76f1e8d192c503367821051c36
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-10 21:33:33 +0530

    Analysis functions - read table, shape

--- ProITBridge/Python/Practice/Chapter4/Analysis 1.py
+++ ProITBridge/Python/Practice/Chapter4/Analysis 1.py
@@ -10,3 +10,8 @@ import pandas as pd
 order_details = pd.read_table(filepath_or_buffer= "data.tsv.txt")
 
 print(order_details.shape)
+
+print(order_details.isnull()) # Entire table with true returned where value is null 
+
+print(order_details.isnull().sum())  # Chain of Functions or Chain of Commands
+# Proper Descripton of null is returned 
