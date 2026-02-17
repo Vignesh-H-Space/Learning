4b35bc94122996c4645189b914fb914b8b61b249
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-17 21:49:24 +0530

    Greeting Message

--- ProITBridge/Python/Tasks/Task 1 - Greeting Message.py
+++ ProITBridge/Python/Tasks/Task 1 - Greeting Message.py
@@ -1,3 +1,7 @@
 # Task - 1 : Greeting Message
 
 # Description - Write a program to take a name and age from the user and print a Greeting Message 
+
+name = input("Enter your Name: ")
+age = int(input("Enter your age: "))
+print(f"Hi {name}, congratulations on being healthy at the age of {age}!!!")
