5e1f71d0f17a1315e0dc6cf6f4154054b43a8146
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-18 05:55:25 +0530

    Zero Division by Try-Except

--- ProITBridge/Python/Tasks/Chapter4/Task 3 - Division by Zero.py
+++ ProITBridge/Python/Tasks/Chapter4/Task 3 - Division by Zero.py
@@ -1,4 +1,10 @@
 # Task 3 - Division by Zero
 
 # A program which handles Division by zero using try-except
-
+print("Program to Divide two numbers")
+num1 = int(input("Enter the first Number : "))
+num2 = int(input("Enter the second Number : "))
+try :
+    print(num1/num2)
+except ZeroDivisionError:       # Exception catch
+    print("Error, Cannot divide by Zero")
