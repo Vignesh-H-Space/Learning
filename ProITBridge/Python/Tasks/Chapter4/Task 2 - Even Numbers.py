00c7f541990a890ecf1415c58953d644eb38cf21
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-18 05:39:22 +0530

    Printing even numbers from list of numbers

--- ProITBridge/Python/Tasks/Chapter4/Task 2 - Even Numbers.py
+++ ProITBridge/Python/Tasks/Chapter4/Task 2 - Even Numbers.py
@@ -1,3 +1,11 @@
 # Task - 2 : Even Numbers 
 
 # Create a list of numbers and print only even numbers using for loop 
+
+list1 = [int(x) for x in input("Enter numbers with space ").split()]
+
+print (f"The list of numbers are : {list1}")
+print("The even numbers present in the list are :",end = " ")
+for val in list1:
+    if val % 2 == 0 :
+        print(val,end = " ")
