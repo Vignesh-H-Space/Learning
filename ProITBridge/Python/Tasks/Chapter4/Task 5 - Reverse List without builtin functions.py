f93c349710784e6db41ddccd80f09594155e4d51
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-19 05:51:20 +0530

    Reverse a list without using the builtin functions

--- ProITBridge/Python/Tasks/Chapter4/Task 5 - Reverse List without builtin functions.py
+++ ProITBridge/Python/Tasks/Chapter4/Task 5 - Reverse List without builtin functions.py
@@ -2,3 +2,12 @@
 
 # Reverse a list without using the builtin functions
 
+list1 = [int(x) for x in input("Enter numbers with space ").split()]
+letter = []
+word = 0
+while len(list1) > 0 :
+    word = list1.pop()
+    letter.append(word)
+print(letter)
+
+
