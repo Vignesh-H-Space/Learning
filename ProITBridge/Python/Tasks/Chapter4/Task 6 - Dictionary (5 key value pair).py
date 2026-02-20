5d9b90a26665fd5939db80d17595a87fae58abf7
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-20 06:18:54 +0530

    Using Dataframes

--- ProITBridge/Python/Tasks/Chapter4/Task 6 - Dictionary (5 key value pair).py
+++ ProITBridge/Python/Tasks/Chapter4/Task 6 - Dictionary (5 key value pair).py
@@ -2,6 +2,10 @@
 
 # Create a Dictionary with atleast 5 Key Value Pair 
 
+import pandas as pd
+
 dict = {"Leagues":["Premier League","Laliga","Serie A","Bundesliga","Ligue1"],"Teams":["Arsenal","Barcelona","Napoli","Bayern","Monaco"],"League Standings":[1,2,5,1,7], "Top Scorer" : ["Gyokeres","Yamal","McTominay","Kane","Silva"],"Trophies":[48,64,29,58,15]}
 
-print(dict)
+print(pd.DataFrame(dict))
+
+
