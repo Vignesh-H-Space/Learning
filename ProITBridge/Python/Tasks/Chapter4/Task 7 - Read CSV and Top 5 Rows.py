19ca42812a6af6d069cf364a4d587423a5741fcd
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-20 06:32:34 +0530

    Read CSV and print top 5 Rows

--- ProITBridge/Python/Tasks/Chapter4/Task 7 - Read CSV and Top 5 Rows.py
+++ ProITBridge/Python/Tasks/Chapter4/Task 7 - Read CSV and Top 5 Rows.py
@@ -1,3 +1,9 @@
 # Task 7 - Read CSV and Top 5 Rows
 
 # Use Pandas to read a CSV file and then print the top 5 Rows 
+
+import pandas as pd 
+
+word = pd.read_csv("Euro_2012_stats_TEAM.csv")
+
+print(word.head())
