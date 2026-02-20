e126c81b80d7e45fe2a7f5f93bcfcf5daed767f4
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-20 22:08:55 +0530

    Filter Rows where Age is greater than 30

--- ProITBridge/Python/Tasks/Chapter4/Task 10 - Filter Rows in DataFrame.py
+++ ProITBridge/Python/Tasks/Chapter4/Task 10 - Filter Rows in DataFrame.py
@@ -1,3 +1,11 @@
 # Task 10 - Filter Rows in DataFrame
 
 # Filter Rows in Dataframe where "Age" is greater than 30
+
+import pandas as pd 
+
+file = pd.read_csv("employee_data.csv")
+
+filt = file[file["Age"] > 30]  # Filter Rows where Age is greater than 30 
+
+print(filt)
