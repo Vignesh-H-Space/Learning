c2886debde361ad0776092f2a623ba434432791d
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-20 06:07:42 +0530

    Dictionary with 5 key value pair

--- ProITBridge/Python/Tasks/Chapter4/Task 6 - Dictionary (5 key value pair).py
+++ ProITBridge/Python/Tasks/Chapter4/Task 6 - Dictionary (5 key value pair).py
@@ -1,3 +1,7 @@
 # Task 6 - Dictionary (5 key value pair)
 
 # Create a Dictionary with atleast 5 Key Value Pair 
+
+dict = {"Leagues":["Premier League","Laliga","Serie A","Bundesliga","Ligue1"],"Teams":["Arsenal","Barcelona","Napoli","Bayern","Monaco"],"League Standings":[1,2,5,1,7], "Top Scorer" : ["Gyokeres","Yamal","McTominay","Kane","Silva"],"Trophies":[48,64,29,58,15]}
+
+print(dict)
