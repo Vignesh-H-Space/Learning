e744aceedb6be4118d3f150bcbc73c5443574c9d
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-20 22:03:41 +0530

    Replace Missing Values in DataFrame

--- ProITBridge/Python/Tasks/Chapter4/Task 9 - Replace Missing Values in DataFrame.py
+++ ProITBridge/Python/Tasks/Chapter4/Task 9 - Replace Missing Values in DataFrame.py
@@ -2,3 +2,14 @@
 
 # Replace all the missing values in Dataframe with column's mean
 
+import pandas as pd 
+
+file = pd.read_csv("employee_data.csv")
+
+# Before Change
+print(file)
+
+file = file.fillna(file.mean(numeric_only=True))        # Fill only numeric values since mean is only posible for numeric
+
+# After Change
+print(file)
