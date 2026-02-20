a721bed74411a779aaae74babe086116a97b0d61
Vignesh H <vignesh.h2004official@gmail.com>
2026-02-20 06:42:53 +0530

    Create Dataframe from  Dictionary

--- ProITBridge/Python/Tasks/Chapter4/Task 8 - Create Dataframe from Dictionary.py
+++ ProITBridge/Python/Tasks/Chapter4/Task 8 - Create Dataframe from Dictionary.py
@@ -2,3 +2,8 @@
 
 # Create a dataframe from Python Dictionary
 
+import pandas as pd
+
+dict = {"Leagues":["Premier League","Laliga","Serie A","Bundesliga","Ligue1"],"Teams":["Arsenal","Barcelona","Napoli","Bayern","Monaco"],"League Standings":[1,2,5,1,7], "Top Scorer" : ["Gyokeres","Yamal","McTominay","Kane","Silva"],"Trophies":[48,64,29,58,15]}
+
+print(pd.DataFrame(dict))
