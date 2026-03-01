7f7e2f58a3ea9e859010833d6d28b4f3b9366b5a
Vignesh H <vignesh.h2004official@gmail.com>
2026-03-01 13:21:55 +0530

    Executable Script File

---
+++ ProITBridge/Python/Practice/Chapter6_OOPS/User_Defined_Module.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # User Defined Module Creation
+
+# In[3]:
+
+
+def addition (a ,b):
+    return a + b
+
+
+# In[4]:
+
+
+def subtraction (a,b):
+    return a - b
+
+
+# In[5]:
+
+
+def multiplication(a,b):
+    return a * b
+
+
+# In[6]:
+
+
+def division (a,b):
+    return a / b
+
+
+# In[ ]:
+
+
+
+
