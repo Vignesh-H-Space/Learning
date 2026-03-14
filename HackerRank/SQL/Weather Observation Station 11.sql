f9ca34ab9a2240cc4d760fbb3c9aadc0fb93ec78
Vignesh H <vignesh.h2004official@gmail.com>
2026-03-14 21:31:48 +0530

    City names starting and ending with consonants

--- HackerRank/SQL/Weather Observation Station 11.sql
+++ HackerRank/SQL/Weather Observation Station 11.sql
@@ -1,2 +1,4 @@
  -- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
 
+select distinct(city)
+from station where SUBSTRING(city,1,1) not in ('A','E','I','O','U'
