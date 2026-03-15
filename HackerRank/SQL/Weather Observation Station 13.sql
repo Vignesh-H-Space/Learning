2bfce41668fa961a6ede5a673fef0f47062e664a
Vignesh H <vignesh.h2004official@gmail.com>
2026-03-15 21:39:12 +0530

    Sum of Latitudes in station

--- HackerRank/SQL/Weather Observation Station 13.sql
+++ HackerRank/SQL/Weather Observation Station 13.sql
@@ -1,1 +1,5 @@
 -- Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345 . Truncate your answer to  decimal places.
+
+SELECT ROUND(SUM(Lat_N), 4)
+FROM STATION
+WHERE Lat_N > 38.7880 AND Lat_N < 137.2345;
