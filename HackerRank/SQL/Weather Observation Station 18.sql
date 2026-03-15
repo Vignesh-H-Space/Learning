57a68e2ec478b7e53491f3047d5328243881ebf6
Vignesh H <vignesh.h2004official@gmail.com>
2026-03-15 22:14:46 +0530

    Task Description

--- HackerRank/SQL/Weather Observation Station 18.sql
+++ HackerRank/SQL/Weather Observation Station 18.sql
@@ -3,3 +3,4 @@
 -- b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
 -- c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
 -- d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+-- Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
