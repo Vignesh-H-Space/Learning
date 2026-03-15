163c1e4d1ec3de627ae243166ba55cc9b6482f47
Vignesh H <vignesh.h2004official@gmail.com>
2026-03-15 22:10:48 +0530

    Smallest Western Latitude

--- HackerRank/SQL/Weather Observation Station 17.sql
+++ HackerRank/SQL/Weather Observation Station 17.sql
@@ -1,1 +1,3 @@
 -- Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7880 . Round your answer to 4 decimal places.
+
+select round(LONG_W,4) from STATION where LAT_N>38.7780 order by LAT_N limit 1;
