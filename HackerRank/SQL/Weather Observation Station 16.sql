832225a74fe0f22dd19edf7da48adba79cbbe44a
Vignesh H <vignesh.h2004official@gmail.com>
2026-03-15 22:07:55 +0530

    Smallest Northern Latitude

--- HackerRank/SQL/Weather Observation Station 16.sql
+++ HackerRank/SQL/Weather Observation Station 16.sql
@@ -1,1 +1,3 @@
 -- Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. Round your answer to 4 decimal places.
+
+SELECT Round(min(Lat_N),4) FROM station where Lat_N>38.7780 ;
