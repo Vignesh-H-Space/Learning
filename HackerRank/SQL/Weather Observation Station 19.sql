cf9e471125bdbc67fb9079b8791c05a9522630b4
Vignesh H <vignesh.h2004official@gmail.com>
2026-03-17 22:08:58 +0530

    Euclidean Distance

--- HackerRank/SQL/Weather Observation Station 19.sql
+++ HackerRank/SQL/Weather Observation Station 19.sql
@@ -1,2 +1,9 @@
 -- Consider P1(a,c) and P2(b,d) to be two points on a 2D plane where (a,b) are the respective minimum and maximum value of Northern Latitude (LAT_N)
 
+SELECT
+    ROUND(SQRT(
+        POWER(MAX(LAT_N)  - MIN(LAT_N),  2)
+      + POWER(MAX(LONG_W) - MIN(LONG_W), 2)
+    ), 4)
+FROM 
+    STATION;
