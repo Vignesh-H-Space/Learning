38e4d3ae56d6b681ff73ea7d5f3bc4ba8234ed29
Vignesh H <vignesh.h2004official@gmail.com>
2026-03-22 16:11:21 +0530

    Table - Teams

--- ProITBridge/SQL/Practice/Chapter 1/schema 1.sql
+++ ProITBridge/SQL/Practice/Chapter 1/schema 1.sql
@@ -1,1 +1,12 @@
 create database FootballDB;
+
+use FootballDB;
+
+-- Teams table
+create table Teams (
+    team_id int auto_increment primary key,
+    team_name varchar(50),
+    city varchar(50),
+    coach_name varchar(50)
+);
+
