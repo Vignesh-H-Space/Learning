90d6d3596e9548ad1b358757bfc7a05bc6081c68
Vignesh H <vignesh.h2004official@gmail.com>
2026-03-22 16:26:37 +0530

    Table - Players

--- ProITBridge/SQL/Practice/Chapter 1/schema 1.sql
+++ ProITBridge/SQL/Practice/Chapter 1/schema 1.sql
@@ -10,3 +10,12 @@ create table Teams (
     coach_name varchar(50)
 );
 
+-- Players table
+create table Players (
+    player_id int auto_increment primary key,
+    first_name varchar(50),
+    last_name varchar(50),
+    position varchar(30),
+    birthdate date,
+    team_id int,
+);
