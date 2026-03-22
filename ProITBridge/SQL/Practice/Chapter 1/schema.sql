f05e3808d0ba4e66479ce06d22a7aefcf06e1927
Vignesh H <vignesh.h2004official@gmail.com>
2026-03-22 14:09:01 +0530

    Creating Table Students

--- ProITBridge/SQL/Practice/Chapter 1/schema.sql
+++ ProITBridge/SQL/Practice/Chapter 1/schema.sql
@@ -1,1 +1,11 @@
-create DATABASE Test1; 
+-- create DATABASE Test1; 
+
+use studentdb ; 
+
+create table Students (
+    studentId int auto_increment primary key ,
+    firstname varchar (20),
+    lastname varchar (20),
+    birthdate date,
+    gender varchar (20)
+);
