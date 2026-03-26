b3a563058d5fc847e0eceee15ed16ee5b23fb41a
Vignesh H <vignesh.h2004official@gmail.com>
2026-03-26 21:36:31 +0530

    Table - ProductLines

--- ProITBridge/SQL/Tasks/Chapter 2/Task.sql
+++ ProITBridge/SQL/Tasks/Chapter 2/Task.sql
@@ -1,2 +1,10 @@
 -- Creating Database - Sales management 
 CREATE DATABASE SalesManagement; 
+
+USE SalesManagement; 
+
+CREATE TABLE ProductLines (ProductLine varchar (30) primary key , textDescription varchar (50), htmlDescription varchar (50), image BLOB ); 
+
+INSERT INTO ProductLines (ProductLine, textDescription, htmlDescription, image) VALUES ('Classic Cars', 'Vintage and classic model cars', NULL, NULL);
+
+INSERT INTO ProductLines (ProductLine, textDescription, htmlDescription, image) VALUES  ('Motorcycles', 'Racing and sports bikes', NULL, NULL);
