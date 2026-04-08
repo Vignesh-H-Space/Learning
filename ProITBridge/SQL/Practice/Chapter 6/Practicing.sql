371a93f46ce64e9999b845871a3828a8609c7b67
Vignesh H <vignesh.h2004official@gmail.com>
2026-04-08 06:23:16 +0530

    Inserting Values into Table Sales Data

--- ProITBridge/SQL/Practice/Chapter 6/Practicing.sql
+++ ProITBridge/SQL/Practice/Chapter 6/Practicing.sql
@@ -5,3 +5,13 @@ USE sales_db;
 
 CREATE TABLE sales_data (transaction_id INT PRIMARY KEY, employee_id INT, month VARCHAR(7), sales_amount DECIMAL (10,2));
 
+INSERT INTO sales_data (transaction_id, employee_id, month, sales_amount) VALUES 
+(1,101,'2024-01',500.00),
+(2,102,'2024-01',600.00),
+(3,101,'2024-02',400.00),
+(4,103,'2024-02',700.0),
+(5,102,'2024-02',650.00),
+(6,101,'2024-03',550.00),
+(7,103,'2024-03',800.00),
+(8,102,'2024-03',700.00),
+(9,104,'2024-03',300.00); 
