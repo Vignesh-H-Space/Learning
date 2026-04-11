0ce638fd77d3966167f8fd99495d86cccddf427f
Vignesh H <vignesh.h2004official@gmail.com>
2026-04-11 13:21:28 +0530

    Creating Tables - Employees & Salaries

---
+++ ProITBridge/SQL/Tasks/Chapter 6/Task.sql
@@ -0,0 +1,12 @@
+-- Window Functions and Stored Procedures 
+
+-- Creating Database 
+
+CREATE DATABASE company;
+
+USE DATABASE company;
+
+-- CREATING TABLES 
+CREATE TABLE employees(employee_id INT PRIMARY KEY, employee_name VARCHAR(20), department VARCHAR (20));
+
+CREATE TABLE salaries(salary_id INT PRIMARY KEY, employee_id INT, month VARCHAR (10), salary decimal(10,2) , FOREIGN KEY (employee_id) REFERENCES employees(employee_id)) ;
