deede15d677c6301021981b67486c4f09a1c00ff
Vignesh H <vignesh.h2004official@gmail.com>
2026-04-11 18:47:36 +0530

    Inserting into Employee table

--- ProITBridge/SQL/Tasks/Chapter 6/Task.sql
+++ ProITBridge/SQL/Tasks/Chapter 6/Task.sql
@@ -10,3 +10,10 @@ USE DATABASE company;
 CREATE TABLE employees(employee_id INT PRIMARY KEY, employee_name VARCHAR(20), department VARCHAR (20));
 
 CREATE TABLE salaries(salary_id INT PRIMARY KEY, employee_id INT, month VARCHAR (10), salary decimal(10,2) , FOREIGN KEY (employee_id) REFERENCES employees(employee_id)) ;
+
+-- INSERTING VALUES INTO TABLE
+INSERT INTO employees VALUES
+(101, 'Arun', 'IT'),
+(102, 'Priya', 'HR'),
+(103, 'Karthik', 'Finance'),
+(104, 'Sneha', 'IT');
