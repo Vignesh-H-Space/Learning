bf4b8e9efa041eec795c120ac3cfee9e32a77715
Vignesh H <vignesh.h2004official@gmail.com>
2026-04-16 22:08:39 +0530

    If else with elif

---
+++ Edube/PCEP/Module 3/if else.py
@@ -0,0 +1,7 @@
+a = input("Enter the plant name ")
+if a == "Spathiphyllum" :
+    print("Spathiphyllum is the best plant ever!")
+elif a == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else :
+    print("Spathiphyllum! Not "+a+"!")
