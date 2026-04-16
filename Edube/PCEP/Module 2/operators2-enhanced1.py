dda5d385085e75bdca4a7c1abdf18dc88da81173
Vignesh H <vignesh.h2004official@gmail.com>
2026-04-16 21:55:11 +0530

    Total hours to minutes

--- Edube/PCEP/Module 2/operators2-enhanced1.py
+++ Edube/PCEP/Module 2/operators2-enhanced1.py
@@ -5,3 +5,6 @@ dura = int(input("Event duration (minutes): "))
 # Step 1: Convert the start time entirely into minutes and add the duration
 total_minutes = (hour * 60) + mins + dura
 
+# Step 2: Extract the final hour 
+# Find total hours (total_minutes // 60), then format to a 24-hour clock (% 24)
+final_hour = (total_minutes // 60) % 24
