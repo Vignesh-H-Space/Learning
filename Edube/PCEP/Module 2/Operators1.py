174140df4a7b34b38f44c8dac63a0aa346695848
Vignesh H <vignesh.h2004official@gmail.com>
2026-04-16 21:32:43 +0530

    Order of Expression

---
+++ Edube/PCEP/Module 2/Operators1.py
@@ -0,0 +1,12 @@
+# Your task is to complete the code in order to evaluate the following expression:
+# 1 /  X + 1 / x + 1 / x + 1 / x
+
+# The result should be assigned to y. Be careful - watch the operators and keep their priorities in mind. Don't hesitate to use as many parentheses as you need.
+
+# You can use additional variables to shorten the expression (but it's not necessary). Test your code carefully.
+
+x = float(input("Enter value for x: "))
+
+y = 1 / (x + 1 / (x + 1 / (x + 1 / x)))
+
+print("y =", y)
