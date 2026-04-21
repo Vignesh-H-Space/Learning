b5a5d53bea3c9711cabaf8750c2b7ea5655b738c
Vignesh H <vignesh.h2004official@gmail.com>
2026-04-21 22:06:28 +0530

    Beatles

---
+++ Edube/PCEP/Module 3/Beatles.py
@@ -0,0 +1,32 @@
+# step 1
+beatles = []
+print("Step 1:", beatles)
+
+# step 2
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print("Step 2:", beatles)
+
+# step 3
+print("Add Stu Sutcliffe and Pete Best to the band ")
+for i in range(2):
+    mem = input("Enter the next member of the Band ")
+    beatles.append(mem)
+    
+print("Step 3:", beatles)
+# step 4
+
+del beatles[-1]
+del beatles[-1]
+
+print("Step 4:", beatles)
+
+# step 5
+
+beatles.insert(0,"Ringo Starr")
+print("Step 5:", beatles)
+
+
+# testing list legth
+print("The Fab", len(beatles))
