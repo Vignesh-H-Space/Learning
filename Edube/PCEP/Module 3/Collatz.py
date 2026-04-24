83e9bfe5d8478775a555d67c84aeabeb1b096797
Vignesh H <vignesh.h2004official@gmail.com>
2026-04-24 22:14:32 +0530

    Collatz Formula - Computation

--- Edube/PCEP/Module 3/Collatz.py
+++ Edube/PCEP/Module 3/Collatz.py
@@ -1,4 +1,5 @@
-''' In 1937, a German mathematician named Lothar Collatz formulated an intriguing hypothesis (it still remains unproven) which can be described in the following way:
+''' In 1937, a German mathematician named Lothar Collatz formulated an intriguing hypothesis 
+(it still remains unproven) which can be described in the following way:
 
 take any non-negative and non-zero integer number and name it c0;
 if it's even, evaluate a new c0 as c0 ÷ 2;
@@ -6,10 +7,13 @@ otherwise, if it's odd, evaluate a new c0 as 3 × c0 + 1;
 if c0 ≠ 1, skip to point 2.
 The hypothesis says that regardless of the initial value of c0, it will always go to 1.
 
-Of course, it's an extremely complex task to use a computer in order to prove the hypothesis for any natural number (it may even require artificial intelligence), but you can use Python to check some individual numbers. Maybe you'll even find the one which would disprove the hypothesis.
+Of course, it's an extremely complex task to use a computer in order to prove the hypothesis for any natural number
+(it may even require artificial intelligence), but you can use Python to check some individual numbers.
+Maybe you'll even find the one which would disprove the hypothesis.
 
 
-Write a program which reads one natural number and executes the above steps as long as c0 remains different from 1. We also want you to count the steps needed to achieve the goal. Your code should output all the intermediate values of c0, too.
+Write a program which reads one natural number and executes the above steps as long as c0 remains different from 1. 
+We also want you to count the steps needed to achieve the goal. Your code should output all the intermediate values of c0, too.
 
 Hint: the most important part of the problem is how to transform Collatz's idea into a while loop - this is the key to success.
 
@@ -39,3 +43,18 @@ Expected output:
 2
 1
 steps = 17 ''' 
+
+
+num = int(input("Enter the number: "))
+if num < 1:
+    print("Non Negative and non-zero numbers only. Try again")
+val = 0
+while num != 1:
+    if num % 2 == 0:
+        num /= 2 
+        print(num)
+    else: 
+        num = 3 * num + 1
+        print(num)
+        
+        
