1e080ab7e05686e7a23d9d0eb8173f83b6f88226
Vignesh H <vignesh.h2004official@gmail.com>
2026-04-24 22:41:56 +0530

    Function with Ordered Parameter

--- Edube/PCEP/Module 4/Function in Python.py
+++ Edube/PCEP/Module 4/Function in Python.py
@@ -1,5 +1,12 @@
-# Function
+# Function with Parameter
 def get_name (fname, lname):
     print("The name of the person is",fname,lname)
 
 get_name("Vijay","Mallayya")
+
+# Function with ordered Parameter
+def get_name2(fname = "Roshan",lname = "Kumar"):
+    print("The second person name is",fname,lname)
+    
+get_name2()
+get_name2("Harshal","Patel")
