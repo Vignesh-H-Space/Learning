64d5858610fb84616f555d90daca9e4119f8ec81
Vignesh H <vignesh.h2004official@gmail.com>
2026-04-28 22:02:34 +0530

    Prime Number using function

---
+++ Edube/PCEP/Module 4/prime number.py
@@ -0,0 +1,16 @@
+def is_prime(num):
+    if num < 2 : 
+        return False
+    
+    for i in range(2,num):
+        if num%i != 0 :
+            return False
+
+            
+    return False
+
+
+for i in range(1, 20):
+	if is_prime(i + 1):
+			print(i + 1, end=" ")
+print()
