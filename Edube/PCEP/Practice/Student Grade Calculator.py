85d05234f5e3fd9f82f4e833a2bab5fde55185a2
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-08 22:03:44 +0530

    Student Grade Calculator

---
+++ Edube/PCEP/Practice/Student Grade Calculator.py
@@ -0,0 +1,14 @@
+def calculate_grade(mark):
+    if mark >= 90:
+        return "A"
+    elif mark >= 75:
+        return "B"
+    elif mark >= 50:
+        return "C"
+    else:
+        return "Fail"
+
+
+score = 82
+
+print("Grade:", calculate_grade(score))
