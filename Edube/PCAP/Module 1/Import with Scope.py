4b880fc1ca71279f92f41e368db50bb76349d80d
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-11 22:40:02 +0530

    Importing with scope function and variables

--- Edube/PCAP/Module 1/Import with Scope.py
+++ Edube/PCAP/Module 1/Import with Scope.py
@@ -1,12 +1,25 @@
+import math
+
 x = 100
 
 def change():
     global x
-    x = x + 50
-    print("Inside:", x)
+    
+    x = x + int(math.sqrt(2500))
+    
+    print("Inside change:", x)
+
+def local_scope():
+    x = 999
+    
+    print("Local x:", x)
 
 print("Before:", x)
 
 change()
 
+local_scope()
+
 print("After:", x)
+
+print("Pi value:", round(math.pi, 2))
