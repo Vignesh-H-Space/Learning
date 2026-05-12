3378f976a05a3382d13c697e4bc9fa3cbb092cdb
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-12 21:42:47 +0530

    Math Module - exp,log

--- Edube/PCAP/Module 1/Math Module.py
+++ Edube/PCAP/Module 1/Math Module.py
@@ -1,4 +1,5 @@
 from math import pi, radians, degrees, sin, cos, tan, asin
+from math import e, exp, log
 
 ad = 90
 ar = radians(ad)
@@ -8,3 +9,8 @@ print(ad == 90.)
 print(ar == pi / 2.)
 print(sin(ar) / cos(ar) == tan(ar))
 print(asin(sin(ar)) == ar)
+
+
+print(pow(e, 1) == exp(log(e)))
+print(pow(2, 2) == exp(2 * log(2)))
+print(log(e, e) == exp(0))
