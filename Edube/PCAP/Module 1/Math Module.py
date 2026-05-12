5c7af132a322dd27527b8dcff2a9b6f8b4c766a1
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-12 21:52:24 +0530

    Math Module - ceil, floor, factorial, hypotenuese

--- Edube/PCAP/Module 1/Math Module.py
+++ Edube/PCAP/Module 1/Math Module.py
@@ -1,5 +1,6 @@
 from math import pi, radians, degrees, sin, cos, tan, asin
 from math import e, exp, log
+from math import ceil, floor, trunc, factorial, hypot
 
 ad = 90
 ar = radians(ad)
@@ -14,3 +15,15 @@ print(asin(sin(ar)) == ar)
 print(pow(e, 1) == exp(log(e)))
 print(pow(2, 2) == exp(2 * log(2)))
 print(log(e, e) == exp(0))
+
+x = 1.4
+y = 2.6
+
+print(floor(x), floor(y))
+print(floor(-x), floor(-y))
+print(ceil(x), ceil(y))
+print(ceil(-x), ceil(-y))
+print(trunc(x), trunc(y))
+print(trunc(-x), trunc(-y))
+print(factorial(5))
+print(hypot(3,4))
