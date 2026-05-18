42a3e256a26dd536139a6895e6dffcecb12cf7c5
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-18 21:32:38 +0530

    Pygame - Initialisation

---
+++ Edube/PCAP/Module 1/Pygame.py
@@ -0,0 +1,10 @@
+import pygame
+
+run = True
+width = 400
+height = 100
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+font = pygame.font.SysFont(None, 48)
+text = font.render("Welcome to pygame", True, (255, 255, 255))
+screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
