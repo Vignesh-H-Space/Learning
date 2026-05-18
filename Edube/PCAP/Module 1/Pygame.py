58a053ce6d9a5bb315ece32784ec7d635af10716
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-18 22:17:57 +0530

    Pygame using Event Handling

--- Edube/PCAP/Module 1/Pygame.py
+++ Edube/PCAP/Module 1/Pygame.py
@@ -22,3 +22,25 @@ text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
 # Main loop
 running = True
 clock = pygame.time.Clock()
+
+
+while running:
+    screen.fill(BG_COLOR)
+
+    # Draw text
+    screen.blit(text, text_rect)
+
+    pygame.display.update()
+
+    # Event handling
+    for event in pygame.event.get():
+        if (
+            event.type == pygame.QUIT
+            or event.type == pygame.KEYUP
+            or event.type == pygame.MOUSEBUTTONUP
+        ):
+            running = False
+
+    clock.tick(60)
+
+pygame.quit()
