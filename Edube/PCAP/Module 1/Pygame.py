1fc91e018e6358219cedbb8908b8ffc954e42087
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-18 21:44:46 +0530

    Pygame - Flip and out condition

--- Edube/PCAP/Module 1/Pygame.py
+++ Edube/PCAP/Module 1/Pygame.py
@@ -8,3 +8,7 @@ screen = pygame.display.set_mode((width, height))
 font = pygame.font.SysFont(None, 48)
 text = font.render("Welcome to pygame", True, (255, 255, 255))
 screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
+pygame.display.flip()
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT\
