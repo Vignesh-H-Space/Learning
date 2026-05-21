5d7433617eb344eb59848fcb59982012359cd620
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-21 21:42:23 +0530

    Center function

---
+++ Edube/PCAP/Module 2/Endswith.py
@@ -0,0 +1,9 @@
+print('[' + 'Beta'.center(2) + ']')
+print('[' + 'Beta'.center(4) + ']')
+print('[' + 'Beta'.center(6) + ']')
+
+# Right Oriented
+print('[' + 'alpha'.center(8) + ']')
+
+print('[' + 'gamma'.center(20, '*') + ']')
+
