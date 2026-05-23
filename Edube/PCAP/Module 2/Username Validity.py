e8eb8ff76dc45379f1a15f92cac97a08d99fc241
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-23 15:48:21 +0530

    Length of Username should be between 6 and 15

--- Edube/PCAP/Module 2/Username Validity.py
+++ Edube/PCAP/Module 2/Username Validity.py
@@ -1,6 +1,6 @@
 username = input("Enter username: ")
 
-if username[0].isalpha() and username.replace("_", "").isalnum() :
+if username[0].isalpha() and username.replace("_", "").isalnum() and 6 <= len(username) <= 15:
     print("Valid Username")
 else:
     print("Invalid Username")
