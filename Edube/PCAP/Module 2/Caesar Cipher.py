526f6ea9fa0476e900104b13e6ac712974583abe
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-28 20:49:00 +0530

    Caesar Cipher program

---
+++ Edube/PCAP/Module 2/Caesar Cipher.py
@@ -0,0 +1,11 @@
+# Caesar cipher.
+text = input("Enter your message: ")
+cipher = ''
+for char in text:
+    if not char.isalpha():
+        continue
+    char = char.upper()
+    code = ord(char) + 1
+    cipher += chr(code)
+
+print(cipher)
