66683e3443fc6ce0e8976d5900edceee2a3ab573
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-29 07:16:10 +0530

    Task Description

---
+++ Edube/PCAP/Module 2/Find a word.py
@@ -0,0 +1,12 @@
+# Let's play a game. We will give you two strings: one being a word (e.g., "dog") and the second being a combination of any characters.
+
+# Your task is to write a program which answers the following question: are the characters comprising the first string hidden inside the second string?
+
+# For example:
+
+# if the second string is given as "vcxzxduybfdsobywuefgas", the answer is yes;
+# if the second string is "vcxzxdcybfdstbywuefsas", the answer is no (as there are neither the letters "d", "o", or "g", in this order)
+# Hints:
+
+# you should use the two-argument variants of the pos() functions inside your code;
+# don't worry about case sensitivity.
