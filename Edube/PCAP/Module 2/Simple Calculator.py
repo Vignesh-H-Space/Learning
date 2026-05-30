cb3faab6615eac3e7310f477e5900b5950edf445
Vignesh H <vignesh.h2004official@gmail.com>
2026-05-30 21:40:16 +0530

    Simple calculator with exception

---
+++ Edube/PCAP/Module 2/Simple Calculator.py
@@ -0,0 +1,19 @@
+try:
+    a = float(input("First number: "))
+    op = input("Operator (+ - * /): ")
+    b = float(input("Second number: "))
+
+    if op == "+":
+        print(a + b)
+    elif op == "-":
+        print(a - b)
+    elif op == "*":
+        print(a * b)
+    elif op == "/":
+        print(a / b)
+    else:
+        print("Invalid operator")
+
+except ValueError:
+    print("Please enter numbers only!")
+
