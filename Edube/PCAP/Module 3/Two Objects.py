33059d171f373a6124ca633b8aaf77b58a3c5a27
Vignesh H <vignesh.h2004official@gmail.com>
2026-06-13 18:13:44 +0530

    Two objects call for same object

--- Edube/PCAP/Module 3/Two Objects.py
+++ Edube/PCAP/Module 3/Two Objects.py
@@ -5,3 +5,13 @@ class Counter:
     def increment(self):
         self.value += 1
 
+c1 = Counter()
+c2 = Counter()
+
+c1.increment()
+c1.increment()
+
+c2.increment()
+
+print(c1.value)
+print(c2.value)
