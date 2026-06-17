c082942fec333140582c3a8e4205e6d5d219d078
Vignesh H <vignesh.h2004official@gmail.com>
2026-06-17 22:11:06 +0530

    hasattr function

--- Edube/PCAP/Module 3/Attribute Existence.py
+++ Edube/PCAP/Module 3/Attribute Existence.py
@@ -5,11 +5,10 @@ class ExampleClass:
         else:
             self.b = 1
 
-example_object = ExampleClass(1)
 
+example_object = ExampleClass(1)
 print(example_object.a)
 
-try:
+if hasattr(example_object, 'b'):
     print(example_object.b)
-except AttributeError:
-    pass
+
