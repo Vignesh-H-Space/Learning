ef627e812b08b545e71a62f213e9f5eaa6959aa6
Vignesh H <vignesh.h2004official@gmail.com>
2026-06-17 22:21:28 +0530

    Instance variable during instantiation

--- Edube/PCAP/Module 3/Instance and private variable.py
+++ Edube/PCAP/Module 3/Instance and private variable.py
@@ -4,3 +4,7 @@ class Sample:
         self.alpha = 1 # Instance variable.
         self.__delta = 3 # Private instance variable.
 
+obj = Sample()
+obj.beta = 2  # Another instance variable (existing only inside the "obj" instance.)
+print(obj.__dict__)
+
