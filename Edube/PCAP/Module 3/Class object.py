76b055c7e41df50308b9ac0b7bc59eef2bbf3506
Vignesh H <vignesh.h2004official@gmail.com>
2026-06-21 15:23:44 +0530

    Object variable

--- Edube/PCAP/Module 3/Class object.py
+++ Edube/PCAP/Module 3/Class object.py
@@ -3,3 +3,9 @@ class ExampleClass:
     def __init__(self, val):
         ExampleClass.varia = val
 
+
+print(ExampleClass.__dict__)
+example_object = ExampleClass(2)
+
+print(ExampleClass.__dict__)
+print(example_object.__dict__)
