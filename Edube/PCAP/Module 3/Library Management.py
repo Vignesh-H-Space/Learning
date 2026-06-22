4ebe5d5bdeaedef3c5e34d79b5da80ed22ac5e4e
Vignesh H <vignesh.h2004official@gmail.com>
2026-06-22 21:22:52 +0530

    Borrow method in Class Book

--- Edube/PCAP/Module 3/Library Management.py
+++ Edube/PCAP/Module 3/Library Management.py
@@ -1,5 +1,13 @@
+
 class Book:
     def __init__(self, title):
         self.title = title
         self.available = True
+
+    def borrow(self):
+        if self.available:
+            self.available = False
+            print(f"{self.title} borrowed")
+        else:
+            print(f"{self.title} is unavailable")
 
