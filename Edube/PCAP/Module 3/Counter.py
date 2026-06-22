8f1c907129151050fe46c03b9e7ff1902ae0f915
Vignesh H <vignesh.h2004official@gmail.com>
2026-06-22 21:53:34 +0530

    Display method and instantiating

--- Edube/PCAP/Module 3/Counter.py
+++ Edube/PCAP/Module 3/Counter.py
@@ -4,4 +4,13 @@ class Counter:
     def __init__(self):
         Counter.total += 1
 
- 
+    def display(self):
+        print(Counter.total)
+
+
+a = Counter()
+b = Counter()
+c = Counter()
+
+a.display()
+b.display()
