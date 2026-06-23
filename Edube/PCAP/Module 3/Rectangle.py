773cb51fec4982e1c67b4ceabe63f854cd4c3505
Vignesh H <vignesh.h2004official@gmail.com>
2026-06-23 22:09:22 +0530

    Method area in class

--- Edube/PCAP/Module 3/Rectangle.py
+++ Edube/PCAP/Module 3/Rectangle.py
@@ -3,3 +3,8 @@ class Rectangle:
         self.length = length
         self.breadth = breadth
 
+    def area(self):
+        return self.length * self.breadth
+
+r = Rectangle(10, 5)
+print("Area =", r.area())
