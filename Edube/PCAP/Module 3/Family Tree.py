8d48865189db48d0b53277e4abf3a2da7bcde3f6
Vignesh H <vignesh.h2004official@gmail.com>
2026-06-29 21:38:16 +0530

    Subclass named Parent

--- Edube/PCAP/Module 3/Family Tree.py
+++ Edube/PCAP/Module 3/Family Tree.py
@@ -2,6 +2,11 @@ class Grandparent:
     def __init__(self):
         self.age = 80
 
+class Parent(Grandparent):
+    def __init__(self):
+        # Notice what is missing here!
+        self.money = 100
+        
 class Child(Parent):
     def __init__(self):
         Parent.__init__(self)
