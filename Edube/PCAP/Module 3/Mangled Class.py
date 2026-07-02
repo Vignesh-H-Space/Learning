2bb6b8b2b371b6151db3ca94eefcd3225f99f90a
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-02 21:55:30 +0530

    Child class which inherits the base class

--- Edube/PCAP/Module 3/Mangled Class.py
+++ Edube/PCAP/Module 3/Mangled Class.py
@@ -5,3 +5,10 @@ class Parent:
     def get_parent_money(self):
         return self.__money
 
+class Child(Parent):
+    def __init__(self):
+        # Successfully build the parent's variables
+        Parent.__init__(self)
+        
+        # The child tries to overwrite the private money variable!
+        self.__money = 500  
