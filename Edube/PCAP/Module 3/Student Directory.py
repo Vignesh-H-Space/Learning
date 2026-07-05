f4b758d37260ef1c946c9866930d33cb056523d7
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-05 15:26:51 +0530

    Instantiation with objects

--- Edube/PCAP/Module 3/Student Directory.py
+++ Edube/PCAP/Module 3/Student Directory.py
@@ -17,3 +17,13 @@ class Student:
         print(self.name)
         print(self.marks)
         print(self.average())
+        
+s1 = Student("Alice")
+s2 = Student("Bob")
+
+for i in [80, 90, 70]:
+    s1.add(i)
+
+for i in [60, 75]:
+    s2.add(i)
+
