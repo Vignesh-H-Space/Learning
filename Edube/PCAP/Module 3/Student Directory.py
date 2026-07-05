2a61ff2b42b5660cf1fcbd4bda01591910be165f
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-05 15:22:22 +0530

    Methods - add, average and display

--- Edube/PCAP/Module 3/Student Directory.py
+++ Edube/PCAP/Module 3/Student Directory.py
@@ -6,3 +6,14 @@ class Student:
         self.name = name
         self.marks = []
         Student.total += 1
+        
+    def add(self, mark):
+        self.marks.append(mark)
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+    def display(self):
+        print(self.name)
+        print(self.marks)
+        print(self.average())
