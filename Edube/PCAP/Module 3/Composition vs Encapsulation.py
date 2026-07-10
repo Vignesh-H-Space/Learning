0b3261fcb1c65346f5a147686e123c68d5b6800c
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-10 07:42:11 +0530

    Class car with default method

--- Edube/PCAP/Module 3/Composition vs Encapsulation.py
+++ Edube/PCAP/Module 3/Composition vs Encapsulation.py
@@ -7,3 +7,8 @@ class Engine:
 
     def get_status(self):
         return self.__status
+
+class Car:
+    def __init__(self):
+        # The Car HAS an Engine (Composition), not IS an Engine
+        self.engine = Engine()
