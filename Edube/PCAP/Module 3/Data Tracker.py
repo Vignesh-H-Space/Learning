37a075705c4e15620fd76f2b4a4b4dcaaf53686c
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-12 21:52:12 +0530

    Static methods for Data Tracker

--- Edube/PCAP/Module 3/Data Tracker.py
+++ Edube/PCAP/Module 3/Data Tracker.py
@@ -6,3 +6,8 @@ class DataTracker:
     def add_via_class(cls, val):
         # 'cls' acts like 'self', but for the Blueprint!
         cls.entries.append(val)
+        
+    @staticmethod
+    def add_via_static(val):
+        # Static methods don't get 'self' OR 'cls'. They are just normal functions!
+        DataTracker.entries.append(val)
