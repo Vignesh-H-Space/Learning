db40df101c6b83c3ae4086c377dfe1445e824750
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-14 21:25:57 +0530

    Temperature Setting using the temp method

--- Edube/PCAP/Module 3/Property Illusion.py
+++ Edube/PCAP/Module 3/Property Illusion.py
@@ -6,3 +6,9 @@ class Thermostat:
     def temp(self):
         return self._temp
 
+    @temp.setter
+    def temp(self, value):
+        if value < 0:
+            self._temp = 0
+        else:
+            self._temp = value
