9c5c65aafee9146275eca7edf26a19766c2c3792
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-14 22:10:42 +0530

    Subclasses with inheriting the main classes

--- Edube/PCAP/Module 3/Super keyword.py
+++ Edube/PCAP/Module 3/Super keyword.py
@@ -8,3 +8,12 @@ class B(A):
     def ping(self):
         # super() looks at the NEXT class in the queue, not necessarily A!
         return "B -> " + super().ping()
+
+class C(A):
+    def ping(self):
+        return "C -> " + super().ping()
+
+class D(B, C):
+    def ping(self):
+        return "D -> " + super().ping()
+
