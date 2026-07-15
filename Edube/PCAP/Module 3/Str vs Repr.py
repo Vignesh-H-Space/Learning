bfe5eba19b2ff2e844ea07a234d52587600ad796
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-15 21:50:57 +0530

    Repr method

--- Edube/PCAP/Module 3/Str vs Repr.py
+++ Edube/PCAP/Module 3/Str vs Repr.py
@@ -2,3 +2,8 @@ class Item:
     def __init__(self, name):
         self.name = name
 
+    def __repr__(self):
+        # __repr__ is meant for developers to see the "raw" object
+        return f"REPR: {self.name}"
+
+
