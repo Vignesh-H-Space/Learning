c5427279484f3f442d3716314e7484d9d7796ee6
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-18 21:10:32 +0530

    Instantiating and display

--- Edube/PCAP/Module 3/Mutable Argument.py
+++ Edube/PCAP/Module 3/Mutable Argument.py
@@ -5,4 +5,12 @@ class DataCollector:
     def add_item(self, item):
         self.data.append(item)
 
+d1 = DataCollector()
+d2 = DataCollector()
 
+d1.add_item("Apple")
+
+d2.add_item("Banana")
+
+print(d1.data)
+print(d2.data)
