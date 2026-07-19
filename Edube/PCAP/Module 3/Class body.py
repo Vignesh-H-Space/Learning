780d6ba60bed112fe18dbccf361c9dc035628f04
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-19 20:56:17 +0530

    Class body execution

---
+++ Edube/PCAP/Module 3/Class body.py
@@ -0,0 +1,11 @@
+print("1. Program Started")
+
+class DatabaseEngine:
+    print("2. Blueprint Loaded")
+    
+    def __init__(self):
+        print("3. Database Connected")
+
+print("4. Middle of Program")
+
+db = DatabaseEngine()
