c8f4331a27997f9683df3451481bcfea45834f33
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-19 17:48:59 +0530

    Differnt exceptions

--- Edube/PCAP/Module 3/Exception Hierarchy.py
+++ Edube/PCAP/Module 3/Exception Hierarchy.py
@@ -4,3 +4,11 @@ class NetworkError(Exception):
 class TimeoutError(NetworkError):
     pass
 
+try:
+    raise TimeoutError("Connection timed out!")
+
+except NetworkError:
+    print("Caught a General Network Error!")
+    
+except TimeoutError:
+    print("Caught a specific Timeout Error!")
