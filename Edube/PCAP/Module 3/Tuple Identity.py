2c6e9def3b30a4446256612caebee54f1c7d2a4f
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-19 20:53:00 +0530

    Instantiation and objects

--- Edube/PCAP/Module 3/Tuple Identity.py
+++ Edube/PCAP/Module 3/Tuple Identity.py
@@ -7,3 +7,11 @@ class Car(Vehicle):
 class Boat: 
     pass
 
+my_car = Car()
+
+check_1 = isinstance(my_car, (Boat, Vehicle))
+
+check_2 = issubclass(Car, (Boat, int))
+
+print(check_1)
+print(check_2)
