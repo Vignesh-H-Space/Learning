b8a9b066d6ac9b1a8c801169066913ac05932956
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-21 21:45:34 +0530

    Left Join

--- Microsoft/DP 700/Alexi Partanen - Course/004 programming languages/wh_dp700_queries.sql
+++ Microsoft/DP 700/Alexi Partanen - Course/004 programming languages/wh_dp700_queries.sql
@@ -9,3 +9,19 @@ from dp700_e004.assignments;
 
 select *
 from dp700_e004.projects;
+
+
+select
+name
+,salary
+,concat('$ ',salary) AS salary_string
+from dp700_e004.employees
+where salary >=75000;
+
+select
+e.name
+,e.department_id
+,d.department_name
+from dp700_e004.employees as e
+left join dp700_e004.departments as d
+on e.department_id = d.department_id
