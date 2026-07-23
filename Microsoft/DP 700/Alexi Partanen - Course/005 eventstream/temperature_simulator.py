a130b91df25f80406ab9c147daa97a4f3919a541
Vignesh H <vignesh.h2004official@gmail.com>
2026-07-23 06:09:08 +0530

    Replacing the placeholders and configuring variables

--- Microsoft/DP 700/Alexi Partanen - Course/005 eventstream/temperature_simulator.py
+++ Microsoft/DP 700/Alexi Partanen - Course/005 eventstream/temperature_simulator.py
@@ -8,3 +8,15 @@ import hashlib
 import random
 import time
 import json
+
+# Replace the placeholders with your Event Hubs connection string and event hub name
+EVENTHUB_NAME = 'esehpnhxzayrvu14rciul9_eh'
+CONNECTION_STR = 'Endpoint=sb://esehpnhxzayrvu14rciul9.servicebus.windows.net/;SharedAccessKeyName=key_32b6eb82-9b23-444b-aaad-7237aedcf71f;SharedAccessKey=KkpUeaWS4FphQyP/REmzoDpZ2XYsOIab3+AEhMsqG00=;EntityPath=esehpnhxzayrvu14rciul9_eh'
+
+
+# Configuration variables
+MIN_TEMPERATURE = 19.0          # Minimum temperature value
+MAX_TEMPERATURE = 20.0          # Maximum temperature value
+COUNTRY = "USA"                 # Country
+CITY = "new york"               # City
+SLEEP_TIME = 3                  # Sleep time before sending next event
